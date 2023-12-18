112b08ef174b4eccc6af049ca5bac98c95f8f4a2
OliverMorla <olivermiguel1129@gmail.com>
2023-12-18 15:39:55 -0500

    refactor: modified userRoutes and controllers to for testing purporses on production

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,41 +1,44 @@
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
-import User from "../lib/models/userModel";
+// import User from "../lib/models/userModel";
 
 const signInUser = async (req: Request, res: Response) => {
   const { email, password } = await req.body;
 
-  try {
-    const user = await User.findOne({
-      email: email,
-    });
+  return res.status(200).json({
+    message: "Account login successfully!",
+  });
+  // try {
+  //   const user = await User.findOne({
+  //     email: email,
+  //   });
 
-    if (!user) {
-      return res.status(400).json({
-        ok: false,
-        message: "User does not exist!",
-      });
-    }
+  //   if (!user) {
+  //     return res.status(400).json({
+  //       ok: false,
+  //       message: "User does not exist!",
+  //     });
+  //   }
 
-    const doesItMatch = bcrypt.compareSync(password, user.password);
+  //   const doesItMatch = bcrypt.compareSync(password, user.password);
 
-    if (!doesItMatch) {
-      return res.status(400).json({
-        ok: false,
-        message: "Password is incorrect!",
-      });
-    }
+  //   if (!doesItMatch) {
+  //     return res.status(400).json({
+  //       ok: false,
+  //       message: "Password is incorrect!",
+  //     });
+  //   }
 
-    return res.status(200).json({
-      ok: true,
-      message: "User logged in successfully!",
-      user: {
-        displayName: user.displayName,
-        email: user.email,
-        avatar: user.avatar,
-      },
-    });
-  } catch (err) {}
+  //   return res.status(200).json({
+  //     ok: true,
+  //     message: "User logged in successfully!",
+  //     user: {
+  //       displayName: user.displayName,
+  //       email: user.email,
+  //       avatar: user.avatar,
+  //     },
+  //   });
+  // } catch (err) {}
 };
 
 const createUser = async (req: Request, res: Response) => {
