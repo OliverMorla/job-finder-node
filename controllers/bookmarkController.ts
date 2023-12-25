5d90af0acd71397f4a3ead2d1fb909e92d4a7ec0
OliverMorla <olivermiguel1129@gmail.com>
2023-12-25 14:31:56 -0500

    refactor: finished add to bookmark function

--- controllers/bookmarkController.ts
+++ controllers/bookmarkController.ts
@@ -3,11 +3,61 @@ import { connectDB } from "../lib/db";
 import User from "../lib/models/userModel";
 
 const addToBookmark = async (req: Request, res: Response) => {
-  const { id } = req.body;
+  const { userId, jobId } = req.body;
 
+  if (userId === "" || jobId === "") {
+    return res.status(400).json({
+      ok: false,
+      message: "Please fill in all fields!",
+    });
+  }
   try {
     await connectDB();
-  } catch (err) {}
+
+    const doesBookmarkExist = await User.findOne({
+      _id: userId,
+      "bookmarks.id": jobId,
+    });
+
+    if (doesBookmarkExist) {
+      return res.status(400).json({
+        ok: false,
+        message: "Job already bookmarked!",
+      });
+    }
+
+    const bookmark = await User.findOne(
+      {
+        _id: userId,
+      },
+      {
+        $push: {
+          bookmarks: {
+            id: jobId,
+            createdAt: Date.now(),
+          },
+        },
+      }
+    );
+
+    if (!bookmark) {
+      return res.status(400).json({
+        ok: false,
+        message: "Something went wrong!",
+      });
+    }
+
+    return res.status(200).json({
+      ok: true,
+      message: "Job bookmarked successfully!",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      ok: false,
+      message: "Something went wrong!",
+      error: err instanceof Error ? err.message : null,
+    });
+  }
 };
 
 const removeFromBookmark = async (req: Request, res: Response) => {
